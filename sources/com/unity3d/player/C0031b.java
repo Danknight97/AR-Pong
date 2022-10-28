5c12f462c76fd4e7a3274ba5d6373dd1f06d77ce
Paras Dua <32988075+Danknight97@users.noreply.github.com>
2022-10-28 11:02:42 +0530

    Delete sources directory

--- sources/com/unity3d/player/C0031b.java
+++
@@ -1,9 +0,0 @@
-package com.unity3d.player;
-
-import android.app.Activity;
-
-/* renamed from: com.unity3d.player.b */
-public interface C0031b {
-    /* renamed from: a */
-    void mo107a(Activity activity, Runnable runnable);
-}
